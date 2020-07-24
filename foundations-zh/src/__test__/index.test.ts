4c5317338a0d3b059d708b5a9858b467d9ab6d8c
Ke Daniel Wu <danielwuke@Kes-MacBook-Pro.local>
2020-07-23 23:45:37 -0400

    init

---
+++ foundations-zh/src/__test__/index.test.ts
@@ -0,0 +1,79 @@
+import { expect } from 'chai';
+import { parseError } from '../index';
+const fixtureStack = `TypeError: Error raised
+  at bar http://192.168.31.8:8000/c.js:2:9
+  at calc http://192.168.31.8:8000/a.js:4:3
+  at foo http://192.168.31.8:8000/b.js:4:1
+  at dsa http://192.168.31.8:8000/a.js:4:3
+  at <anonymous>:1:11
+  at http://192.168.31.8:8000/a.js:22:3
+`
+const fixtureFirefoxStack = `
+  bar@http://192.168.31.8:8000/c.js:2:9
+  foo@http://192.168.31.8:8000/b.js:4:15
+  calc@http://192.168.31.8:8000/a.js:4:3
+  <anonymous>:1:11
+  http://192.168.31.8:8000/a.js:22:3
+`
+
+describe('parseError test', () => {
+  it('should contain correct message', () => {
+    const err = new Error('test');
+    const errLog = parseError(err);
+    expect(errLog.message).to.equal('test');
+  });
+
+  it('should parse correctly for chrome stack', () => {
+    const err = new Error('test');
+    err.stack = fixtureStack;
+    const errLog = parseError(err);
+    expect(errLog.message).to.equal('test');
+    expect(errLog.stack.length).to.equal(5);
+    //content check
+    expect(errLog.stack[0].line).to.equal(2);
+    expect(errLog.stack[0].column).to.equal(9);
+    expect(errLog.stack[0].filename).to.equal("http://192.168.31.8:8000/c.js");
+
+    expect(errLog.stack[1].line).to.equal(4);
+    expect(errLog.stack[1].column).to.equal(3);
+    expect(errLog.stack[1].filename).to.equal("http://192.168.31.8:8000/a.js");
+
+    expect(errLog.stack[2].line).to.equal(4);
+    expect(errLog.stack[2].column).to.equal(1);
+    expect(errLog.stack[2].filename).to.equal("http://192.168.31.8:8000/b.js");
+
+    expect(errLog.stack[3].line).to.equal(4);
+    expect(errLog.stack[3].column).to.equal(3);
+    expect(errLog.stack[3].filename).to.equal("http://192.168.31.8:8000/a.js");
+
+    expect(errLog.stack[4].line).to.equal(22);
+    expect(errLog.stack[4].column).to.equal(4);
+    expect(errLog.stack[4].filename).to.equal("http://192.168.31.8:8000/a.js");
+  });
+
+  it('should parse correctly for firefox stack', () => {
+    const err = new Error('test');
+    err.stack = fixtureFirefoxStack;
+    const errLog = parseError(err);
+    expect(errLog.message).to.equal('test');
+    expect(errLog.stack.length).to.equal(4);
+    //content check
+    expect(errLog.stack[0].line).to.equal(2);
+    expect(errLog.stack[0].column).to.equal(9);
+    expect(errLog.stack[0].filename).to.equal("http://192.168.31.8:8000/c.js");
+
+    expect(errLog.stack[1].line).to.equal(4);
+    expect(errLog.stack[1].column).to.equal(15);
+    expect(errLog.stack[1].filename).to.equal("http://192.168.31.8:8000/b.js");
+
+    expect(errLog.stack[2].line).to.equal(4);
+    expect(errLog.stack[2].column).to.equal(3);
+    expect(errLog.stack[2].filename).to.equal("http://192.168.31.8:8000/a.js");
+
+    expect(errLog.stack[3].line).to.equal(22);
+    expect(errLog.stack[3].column).to.equal(3);
+    expect(errLog.stack[3].filename).to.equal("http://192.168.31.8:8000/a.js");
+
+  });
+
+});
--- foundations-zh/src/index.ts
+++ foundations-zh/src/index.ts
@@ -7,6 +7,41 @@ export interface ErrorMessage {
   }>
 }
 
+// 这里用Array存放可能的stacktrace格式
+// 实际情况里可能直接存成CONST然后根据浏览器的类型调用对应的会比较好
+const PATTERNS = [
+  /^(at)\s(?<function>(.*)\s)?(?<path>[^\s]+\.\w+):(?<line>\d+):(?<col>\d+)$/, 
+  /^((?<function>.+)@)?(?<path>[^\s]+\.\w+):(?<line>\d+):(?<col>\d+)$/,
+]
+
+
 export function parseError(err: Error): ErrorMessage {
-  // implement
+  const parsedErrorLog = {
+    message: err.message,
+    stack: [] as any
+  }
+  if (err.stack) {
+    err.stack.split('\n').forEach((row) => {
+      let match = null;
+      for (let i=0; i<PATTERNS.length; i++) {
+        match = PATTERNS[i].exec(row.trim());
+        if(match) {
+          break;
+        }
+      }
+      // 根据要求仅有带文件路径的trace需要被添加进上报记录里
+      if (match) {
+        const groups = match.groups;
+        if (groups) {
+          const stackItem = {
+            line: Number(groups.line) || 0,
+            column: Number(groups.col) || 0,
+            filename: groups.path || ''
+          };
+          parsedErrorLog.stack.push(stackItem);
+        }
+      }
+    })
+  }
+  return parsedErrorLog;
 }
